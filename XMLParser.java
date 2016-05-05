9a1a12c9a2b37a33fcf3bae47954f29e4f6e572b
PAUL EASTERBROOKS <easterpa@dukes.jmu.edu>
2016-05-04 22:25:32 -0400

    adds in a new XML parsing class tvice

---
+++ XMLParser.java
@@ -0,0 +1,12 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * Provides a class to parse XML from the zillow site
+ */
+public class XMLParser {
+
+	public XMLParser() {
+
+	}
+}
